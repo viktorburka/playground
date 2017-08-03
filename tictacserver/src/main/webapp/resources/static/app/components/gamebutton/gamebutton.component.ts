f5033dd429f3ec966ed6129e8fc4c90ad2528ee4
Viktor Burka <viktor.cpp@gmail.com>
2017-08-03 16:37:59 -0400

    Removed commented code

--- tictacserver/src/main/webapp/resources/static/app/components/gamebutton/gamebutton.component.ts
+++ tictacserver/src/main/webapp/resources/static/app/components/gamebutton/gamebutton.component.ts
@@ -18,16 +18,8 @@ import 'rxjs/add/operator/map';
 export class GameButton {
     @Input() position: string;
 
-    //actions: PlayerAction[];
-    //data: Observable<PlayerAction[]>
-
     constructor(private gameLogic: GameLogic,
                 private http: Http) {
-        // this.data = this.http.get('http://localhost:8080/TicTacServer-1.0/services/Rest/products')
-        //                      .map(res => res.json())
-        //                      .map(actions => actions.map( (r: any) => new PlayerAction(r.command, r.commandParameter) ));
-
-        //this.d = this.http.get('http://localhost:8080/TicTacServer-1.0/services/Rest/products');
     }
 
     getBackgroundColor(): string {
@@ -38,16 +30,6 @@ export class GameButton {
         console.log("Position: " + this.position);
         this.gameLogic.onLastPositionChanged(this.position);
         if (this.gameLogic.state == GameResult.Continue) {
-            // this.data.subscribe(
-            //     data => {
-            //         this.actions = data;
-            //         console.log(this.actions);
-            //     },
-            //     err => {
-            //         console.log("Error querying player action. Code: %s, url: %s", err.status, err.url);
-            //     },
-            //     () => console.log("Data received")
-            // );
             this.http.get('http://localhost:8080/TicTacServer-1.0/services/Rest/products')
                      .map(res => res.json())
                      .subscribe(d => { console.log("command: " + d.command + ", parameter: " + d.commandParameter); });
